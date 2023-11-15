03de53793e73616394bb1123929a40b1447be636
Dylan <dylanryan431@gmail.com>
2023-11-15 15:26:24 -0400

    Added Functionality

--- MiniGameLoop/Program.cs
+++ MiniGameLoop/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MiniGameLoop
 {
@@ -12,7 +13,6 @@ namespace MiniGameLoop
         {
             Console.WriteLine("MiniGame");
             Console.WriteLine();
-
             //for loops
             //count 1 to 10
             //for(int i=1; i <=10; i++) //i++ = ( i = i + 1)
@@ -33,44 +33,67 @@ namespace MiniGameLoop
             bool gameOver = false;
             int x = 30; //player position
             int y = 10; //player position
+                       
 
-                //game loop
-            while(gameOver != true) // !=   =   is not true
+            while (gameOver != true) // !=   =   is not true
             {
                 //2. update -- in unity == Update
                 ConsoleKeyInfo input;
                 input = Console.ReadKey(true);
                 if(input.Key == ConsoleKey.W)
                 {
+                    PrintMap();
                     y = y - 1;
-                    if (y < 0) y = 0;
+                    if (y < 2) y = 2;
                 }
                 if(input.Key == ConsoleKey.S)
                 {
+                    PrintMap();
                     y = y + 1;
-                    if (y < 0) y = 0;
                     if (y > 20) y = 20;
                 }
                 if (input.Key == ConsoleKey.D)
                 {
+                    PrintMap();
                     x = x + 1;
-                    if (x < 0) x = 0;
                     if (x > 60) x = 60;
                 }
                 if(input.Key == ConsoleKey.A)
                 {
+                    PrintMap();
                     x = x - 1;
                     if (x < 0) x = 0;
                 }
+                //game loop
                 //3. draw -- in unity auto happens
                 Console.SetCursorPosition(x, y); // player position
                 Console.WriteLine(avatar); //player avatar
-
             }
 
             Console.WriteLine();
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey(true);
         }
+
+        static void PrintMap()
+        {
+            string path = @"map.txt";
+            string[] MapRows;
+            MapRows = File.ReadAllLines(path);
+            for (int i = 0; i < MapRows.Length; i++)
+            {
+                string MapRow = MapRows[i];
+                for (int j = 0; j < MapRow.Length; j++)
+                {
+                    //Console.WriteLine("map(" + x + "," + y + ") = " + tile);  //DEBUG
+
+                    char tile = MapRow[j];
+                    Console.Write(tile);
+                    //map[x,y] = tile
+                }
+                Console.WriteLine();
+            }
+        }
+
     }
 }
